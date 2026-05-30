4b9043396c212ca6c89ce7e7bb3e8a540bda8311
Jeevasurya <kvpfamily2025@gmail.com>
2026-05-30 21:40:39 +0530

    style: remove header back button from Mobile Recharge page

--- src/pages/MobileRecharge.tsx
+++ src/pages/MobileRecharge.tsx
@@ -3,7 +3,7 @@ import { MobileRechargeForm } from '@/components/recharge/MobileRechargeForm';
 
 const MobileRechargePage = () => {
   return (
-    <Layout title="Mobile Recharge" showBack>
+    <Layout title="Mobile Recharge">
       <div className="px-4 py-4 w-full max-w-full box-border">
         <MobileRechargeForm />
       </div>
